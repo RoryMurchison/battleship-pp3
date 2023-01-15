98ccdd59dc8ed9321e9514f8654bdfd01be443b7
RoryMurchison <roryjmurchison@gmail.com>
2023-01-15 23:32:06 +0000

    create main function and print opening text

---
+++ run.py
@@ -0,0 +1,8 @@
+def main()
+    """ Function that runs the game loop """
+
+    print("Welcome to Battleships")
+    print("Can you destroy the 8 enemy ships within 50 shots?")
+    print(". is water, # is a missed shot, X is a hit")
+    print("Fire at will!")
+    print("-----------------------------")
