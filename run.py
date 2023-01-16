c6454cf8ae3a0e33b162b834b6bc55e7d526040b
RoryMurchison <roryjmurchison@gmail.com>
2023-01-16 00:07:15 +0000

    add comments in the create_starting_grid function to better understand code, set while condition for placing ships

--- run.py
+++ run.py
@@ -10,11 +10,12 @@ game_finished = False
 
 def create_starting_grid():
     """ Creates a 10 x 10 grid for the game to be played on """ 
-    global grid
     global size_of_grid
-    global ships_in_game
+    global grid
     global ship_positions
+    global ships_in_game
 
+# Creating the grid itself
     rows, columns = (size_of_grid, size_of_grid)
 
     grid = []
@@ -23,6 +24,12 @@ def create_starting_grid():
         for c in range(columns):
             row.append(".")
         grid.append(row)
+
+# Place the ships to be guessed on the grid
+    ships_placed = 0
+    ship_positions = []
+
+    while ships_placed != ships_in_game:
 
 
 
